884f5dd1cf6bd4af4acdeb9272c2562a6cb9f824
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-04 23:59:59 -0700

    creat method append for junglebeat class

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -2,6 +2,15 @@ class JungleBeat
   attr_reader :list
   def initialize
     @list = LinkedList.new
-    
+  end
+
+  def append(new_data)
+    new_data = new_data.split
+    new_string=  []
+    new_data.each do |data|
+    new_string  << list.append(data)
+    new_string.join(" ")
+    end
+    new_string.join(" ")
   end
 end
