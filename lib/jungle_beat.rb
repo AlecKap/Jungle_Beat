354e632bbb25bde78e41eb33f8a91da2ff2c7509
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-07 11:15:38 -0700

    Add prepend method.

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,5 +1,5 @@
 class JungleBeat
-  attr_reader :list
+  attr_reader :list, :all
   attr_accessor :rate, :voice
   def initialize(data = nil)
     @list = LinkedList.new
@@ -21,6 +21,21 @@ class JungleBeat
     end
     new_string.each do |data|
       @list.append(data)
+    end
+    new_string.join(" ")
+  end
+
+  def prepend(new_data)
+    new_data = new_data.split
+    new_string =  []
+    valid_beats = ["tee", "dee", "deep", "scoopity", "boopity", "doopity", "bop", "woo", "hoo", "shu", "boop", "la", "dop", "doo", "ditt", "na"]
+    new_string = new_data.select do |data|
+      valid_beats.any? do |beat|
+        beat == data
+      end
+    end
+    new_string.each do |data|
+      @list.prepend(data)
     end
     new_string.join(" ")
   end
