830c054118b2943e39bf580040419d7c5c20d83b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-06 19:14:16 -0700

    Modify append method.

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,17 +1,26 @@
 class JungleBeat
   attr_reader :list
   attr_accessor :rate
-  def initialize()
+  def initialize(data = nil)
     @list = LinkedList.new
     @rate = 500
+    @voice = "Boing"
+    if data != nil
+      append(data)
+    end
   end
 
   def append(new_data)
     new_data = new_data.split
-    new_string=  []
-    new_data.each do |data|
-    new_string  << list.append(data)
-    new_string.join(" ")
+    new_string =  []
+    valid_beats = ["tee", "dee", "deep", "bop", "woo", "hoo", "shu", "boop", "la", "doo", "ditt", "na"]
+    new_string = new_data.select do |data|
+      valid_beats.any? do |beat|
+        beat == data
+      end
+    end
+    new_string.each do |data|
+      @list.append(data)
     end
     new_string.join(" ")
   end
@@ -21,10 +30,11 @@ class JungleBeat
   end
 
   def play
-    `say -r #{@rate} -v Boing #{@list.to_string}`
+    `say -r #{@rate} -v #{@voice} #{@list.to_string}`
   end
 
   def rate(num)
     @rate = num
   end
+
 end
